2bbc7b867181ae44095498d22eb68dbd26ce3927
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-02-26 14:54:23 +0100

    Added set and delete methods to the interface

--- library/Interfaces/SearchBackendInterface.php
+++ library/Interfaces/SearchBackendInterface.php
@@ -2,6 +2,28 @@
 
 namespace Imbo\MetadataSearch\Interfaces;
 
+/**
+ * Metadata search backend interface
+ *
+ * @author Kristoffer Brabrand <kristoffer@brabrand.net>
+ */
 interface SearchBackendInterface {
+    /**
+     * Set metadata for an image in the metadata search backend
+     *
+     * @param string $publicKey Public key the image belongs to
+     * @param string $imageIdentifier Image identifier
+     * @param array $metadata Metadata array containing key value pairs
+     * @return bool
+     */
+    public function set($publicKey, $imageIdentifier, array $metadata);
 
+    /**
+     * Delete metadata for an image in the metadata search backend
+     *
+     * @param string $publicKey Public key the image belongs to
+     * @param string $imageIdentifier Image identifier
+     * @return bool
+     */
+    public function delete($publicKey, $imageIdentifier);
 }
