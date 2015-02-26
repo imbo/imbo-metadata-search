46129063cf665f1cac4a5bd8be9915179ff35eab
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-02-26 14:55:00 +0100

    Added empty set and delete methods to the ES backend implementation to comply with interface

--- library/Backend/ElasticSearch.php
+++ library/Backend/ElasticSearch.php
@@ -5,5 +5,17 @@ namespace Imbo\MetadataSearch\Backend;
 use Imbo\MetadataSearch\Interfaces\SearchBackendInterface;
 
 class ElasticSearch implements SearchBackendInterface {
+    /**
+     * {@inheritdoc}
+     */
+    public function set($publicKey, $imageIdentifier, array $metadata) {
 
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($publicKey, $imageIdentifier) {
+
+    }
 }
