d303af42794c50d6e4ce9295ecb3bcde304020d6
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-02-26 19:42:45 +0100

    Pass Elasticsearch\Client to elasticsearch backend

--- config/config.dist.php
+++ config/config.dist.php
@@ -1,10 +1,13 @@
 <?php
+
+namespace Imbo\MetadataSearch;
+
 return [
     'eventListeners' => [
         'metadata' => [
             'listener' => 'Imbo\MetadataSearch\EventListener\MetadataOperations',
             'params' => [
-                'backend' => new Imbo\MetadataSearch\Backend\ElasticSearch(),
+                'backend' => new Backend\ElasticSearch(new \Elasticsearch\Client()),
             ],
         ],
     ],
