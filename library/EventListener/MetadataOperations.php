9339cebd7769c4c8b17881ae0bd06226ac365165
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-02-27 11:38:11 +0100

    Implemented a possibly partially working search event handler

--- library/EventListener/MetadataOperations.php
+++ library/EventListener/MetadataOperations.php
@@ -6,6 +6,7 @@ use Imbo\EventListener\ListenerInterface;
 use Imbo\EventManager\EventInterface;
 use Imbo\Exception\InvalidArgumentException;
 use Imbo\MetadataSearch\Interfaces\SearchBackendInterface;
+use Imbo\MetadataSearch\Dsl\Parser as DslParser;
 
 class MetadataOperations implements ListenerInterface {
     /**
@@ -32,6 +33,7 @@ class MetadataOperations implements ListenerInterface {
             'metadata.post'   => ['post' => -1000],
             'metadata.put'    => ['put' => -1000],
             'metadata.delete' => ['delete' => -1000],
+            'metadata.search' => 'search',
         ];
     }
 
@@ -69,5 +71,43 @@ class MetadataOperations implements ListenerInterface {
         $request = $event->getRequest();
 
         $this->backend->delete($request->getPublicKey(), $request->getImageIdentifier());
+    }
+
+    /**
+     * Handle metadata search operation - return list of imageIdentifers
+     *
+     * page     => Page number. Defaults to 1
+     * limit    => Limit to a number of images pr. page. Defaults to 20
+     * metadata => Whether or not to include metadata pr. image. Set to 1 to enable
+     * query    => urlencoded json data to use in the query
+     * from     => Unix timestamp to fetch from
+     * to       => Unit timestamp to fetch to
+     *
+     * @param Imbo\EventListener\ListenerInterface $event The current event
+     * @param string[] Array with image identifiers
+     */
+    public function search(EventInterface $event) {
+        $request = $event->getRequest();
+
+        $params = $request->query;
+        $query = $request->getContent();
+
+        $publicKey = $request->getPublicKey();
+
+        $queryParams = [
+            'page' => $params->get('page', 1),
+            'limit' => $params->get('limit', 20),
+            'from' => $params->get('from'),
+            'to' => $params->get('to'),
+        ];
+
+        // Parse the query JSON and transform it to an AST
+        $ast = DslParser::parse($query);
+
+        // Query backend using the AST
+        $imageIdentifiers = $this->backend->search($publicKey, $ast, $queryParams);
+
+        // Modify the request parameters on the event here to include an explicit
+        // list of ids to fetch from the database backend using db.images.load
     }
 }
