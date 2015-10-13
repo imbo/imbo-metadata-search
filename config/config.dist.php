e07718958ae830a0dee3a26323b1e4cd81c0b319
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-10-13 23:39:18 +0200

    Remove access control class from metadata. No longer needed (with the latest versions of imbo

--- config/config.dist.php
+++ config/config.dist.php
@@ -4,7 +4,6 @@ namespace Imbo\MetadataSearch;
 
 return [
     'eventListeners' => [
-        'metadata-access-token' => new EventListener\AccessToken(),
         'metadata' => [
             'listener' => new EventListener\MetadataOperations([
                 'backend' => new Backend\ElasticSearch(new \Elasticsearch\Client())
