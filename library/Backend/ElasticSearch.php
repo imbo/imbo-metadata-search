8567162c9aa9000dbd74e222b71232b71cf19eb6
Sindre Gulseth <sgulseth@gmail.com>
2015-12-09 16:31:16 +0100

    Added use Exception so it handle errors

--- library/Backend/ElasticSearch.php
+++ library/Backend/ElasticSearch.php
@@ -7,7 +7,8 @@ use Imbo\MetadataSearch\Dsl\Transformations\ElasticSearchDsl,
     Imbo\MetadataSearch\Interfaces\DslAstInterface,
     Imbo\MetadataSearch\Model\ElasticsearchResponse,
     Elasticsearch\Client as ElasticsearchClient,
-    Imbo\Exception\RuntimeException;
+    Imbo\Exception\RuntimeException,
+    Exception;
 
 /**
  * Elasticsearch search backend for metadata search
@@ -101,7 +102,7 @@ class ElasticSearch implements SearchBackendInterface {
 
         try {
             $queryResult = $this->client->search($params);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             throw new RuntimeException('Metadata search failed: ' . $e->getMessage(), 503);
         }
 
